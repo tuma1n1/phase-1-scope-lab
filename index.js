518eb06c956a0f4f3a7ee61b25f92548dc09f00d
Tumaini <tumainikebaya.234@gmail.com>
2024-04-02 21:51:40 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName(){
+    return customerName.toUpperCase();
+}
+upperCaseCustomerName();
+
+function setBestCustomer(){
+    bestCustomer = "not bob";
+}
+
+function overwritesBestCustomer(){
+    bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "Paul";
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "riantoz";
+}
